652ce576a3227a232e06381752f1bb01d9d88a86
Vivid <vividbloop@gmail.com>
2020-04-10 19:24:33 -0400

    thoughts/roadmap

---
+++ scratch.py
@@ -0,0 +1,15 @@
+
+
+# Connect to Youtube API
+
+
+
+# Run a search for videos
+# YouTube.Search.list('snippet['thumbnails']['url']', {q: '<query>'})
+
+# Select the top video
+# search[0]
+
+# Select the top video's URL
+# video.snippet.thumbnails.(key).url
+# Return the URL
